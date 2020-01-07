768a8799b34df95bff7ed8c251313d3c25eecea3
JQiang <wangjq.email@qq.com>
2020-01-07 11:47:27 +0800

    代码首次提交

---
+++ wangjq-core/src/main/java/com/Application.java
@@ -0,0 +1,13 @@
+package com.core.wangjq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WangjqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WangjqApplication.class, args);
+	}
+
+}
---
+++ wangjq-core/src/test/java/com/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.wangjq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WangjqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
