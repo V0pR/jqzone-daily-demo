541dd250ae94fba756ca38eba3d86acab8e49b93
JQiang <wangjq.email@qq.com>
2020-01-10 14:08:28 +0800

    基础配置

---
+++ wangjq-core/src/main/java/com/core/service/UserService.java
@@ -0,0 +1,12 @@
+package com.core.service;
+
+/**
+ * @author wjq
+ * @version 1.0
+ * @Title: UserService
+ * @Package com.core.service
+ * @Description:
+ * @date 2020/1/10 14:06
+ */
+public interface UserService {
+}
---
+++ wangjq-core/src/main/java/com/core/service/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.core.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author wjq
+ * @version 1.0
+ * @Title: UserServiceImpl
+ * @Package com.core.service
+ * @Description:
+ * @date 2020/1/10 14:07
+ */
+@Service
+public class UserServiceImpl implements UserService {
+}
