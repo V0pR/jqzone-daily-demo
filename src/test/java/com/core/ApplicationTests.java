77625c42465b34888b514c14b02ae24787ec6cdf
wjqvopr <wjq.email@foxmail.com>
2021-01-08 11:05:22 +0800

    .gitignore

--- src/test/java/com/core/ApplicationTests.java
+++ src/test/java/com/core/ApplicationTests.java
@@ -4,7 +4,6 @@ import com.core.domain.User;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
