9a216d33c67cfd54d927d6cef273f20b31ebe125
jqqqqqq <wjq.email@foxmail.com>
2021-03-21 17:35:24 +0800

    add index

---
+++ src/main/java/com/core/controller/IndexController.java
@@ -0,0 +1,31 @@
+package com.core.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author v0pr
+ */
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+
+    /**
+     * 隐私政策
+     * @return
+     */
+    @RequestMapping("/privacy-policy")
+    public String privacyPolicy() {
+        return "privacyPolicy";
+    }
+
+    /**
+     * 用户协议
+     * @return
+     */
+    @RequestMapping("/user-agreement")
+    public String UserAgreement() {
+        return "UserAgreement";
+    }
+
+}
